0866a93528bf0346c0ecf0933a9d6af43c21f2b5
Nenad <shonad@elfa.rs>
2025-08-05 23:06:12 +0200

    Postavljeni pocetni fajlovi

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+Naziv projekta: EBEC sajt
+Autor: Nenad Ignjatović
+Opis: Jednostavan sajt za predstavljanje EBEC takmičenja sa prijavom, galerijom i opisom.
+Spoljašnje biblioteke: Nisu korišćene, za sada.
---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('prijava-forma')?.addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('uspeh').style.display = 'block';
+});
+
+function openModal(img) {
+  document.getElementById('modal').style.display = 'flex';
+  document.getElementById('modal-img').innerHTML = '<img src="' + img.src + '" style="max-width:90vw; max-height:90vh;">';
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = 'none';
+}
