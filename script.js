b89e8a7381d43435e3c90ed07371dc4249ee0fa5
Nenad <shonad@elfa.rs>
2025-08-07 01:32:25 +0200

    Prva verzija pocetne stranice

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.getElementById('prijava-forma')?.addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('uspeh').style.display = 'block';
-});
+// document.getElementById('prijava-forma')?.addEventListener('submit', function(e) {
+//   e.preventDefault();
+//   document.getElementById('uspeh').style.display = 'block';
+// });
 
-function openModal(img) {
-  document.getElementById('modal').style.display = 'flex';
-  document.getElementById('modal-img').innerHTML = '<img src="' + img.src + '" style="max-width:90vw; max-height:90vh;">';
-}
+// function openModal(img) {
+//   document.getElementById('modal').style.display = 'flex';
+//   document.getElementById('modal-img').innerHTML = '<img src="' + img.src + '" style="max-width:90vw; max-height:90vh;">';
+// }
 
-function closeModal() {
-  document.getElementById('modal').style.display = 'none';
-}
+// function closeModal() {
+//   document.getElementById('modal').style.display = 'none';
+// }
+
+
